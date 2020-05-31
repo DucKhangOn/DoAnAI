e16442bc9a0281d91e52ace46afa7e8aaffe2d60
Ngoc Thanh <yasuo12091999@gmail.com>
2020-05-31 14:40:11 +0700

    dkdkdkdkdkdmd

--- DoAnAI/DoAnAI/Form1.Designer.cs
+++ DoAnAI/DoAnAI/Form1.Designer.cs
@@ -28,13 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.readFileButton = new System.Windows.Forms.Button();
+            this.linkTextBox = new System.Windows.Forms.TextBox();
+            this.goalLinkTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // readFileButton
+            // 
+            this.readFileButton.Location = new System.Drawing.Point(325, 244);
+            this.readFileButton.Name = "readFileButton";
+            this.readFileButton.Size = new System.Drawing.Size(105, 66);
+            this.readFileButton.TabIndex = 0;
+            this.readFileButton.Text = "Đọc file";
+            this.readFileButton.UseVisualStyleBackColor = true;
+            this.readFileButton.Click += new System.EventHandler(this.readFileButton_Click);
+            // 
+            // linkTextBox
+            // 
+            this.linkTextBox.Location = new System.Drawing.Point(166, 21);
+            this.linkTextBox.Multiline = true;
+            this.linkTextBox.Name = "linkTextBox";
+            this.linkTextBox.Size = new System.Drawing.Size(407, 166);
+            this.linkTextBox.TabIndex = 1;
+            // 
+            // goalLinkTextBox
+            // 
+            this.goalLinkTextBox.Location = new System.Drawing.Point(325, 204);
+            this.goalLinkTextBox.Name = "goalLinkTextBox";
+            this.goalLinkTextBox.Size = new System.Drawing.Size(105, 20);
+            this.goalLinkTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(266, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Đích đến";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(739, 322);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.goalLinkTextBox);
+            this.Controls.Add(this.linkTextBox);
+            this.Controls.Add(this.readFileButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button readFileButton;
+        private System.Windows.Forms.TextBox linkTextBox;
+        private System.Windows.Forms.TextBox goalLinkTextBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- DoAnAI/DoAnAI/Form1.cs
+++ DoAnAI/DoAnAI/Form1.cs
@@ -3,18 +3,161 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DoAnAI
 {
+
     public partial class Form1 : Form
     {
+        public struct Node
+        {
+            public string PreLink;
+            public string CurLink;
+        };
+        List<Node> Queue;
+        List<Node> Path;
         public Form1()
         {
             InitializeComponent();
+            Queue = new List<Node>();
+            Path = new List<Node>();
         }
+
+        private void readFileButton_Click(object sender, EventArgs e)
+        {
+            linkTextBox.Clear();
+            var flag = true;
+            int index = 0;
+            var s = docketqua(@"C:\Users\Thanh\Desktop\Test\1.html");
+            foreach (var x in s)
+            {
+                Queue.Add(new Node() { PreLink = @"C:\Users\Thanh\Desktop\Test\1.html", CurLink = @"C:\Users\Thanh\Desktop\Test\" + x });
+            }
+            while (flag)
+            {
+                int end = Queue.Count;
+                if (index >= end)
+                {
+                    break;
+                }
+                var temp = docketqua(Queue[index].CurLink);
+                if (CheckInPath(Queue[index].CurLink, Path))
+                {
+                    Path.Add(Queue[index]);
+                }
+                if (temp.Count > 0)
+                {
+                    foreach (var t in temp)
+                    {
+                        if (CheckInPath(t, Path) == true)
+                        {
+                            Queue.Add(new Node() { PreLink = Queue[index].CurLink, CurLink = @"C:\Users\Thanh\Desktop\Test\" + t });
+                        }
+                        if (t == goalLinkTextBox.Text)
+                        {
+                            Path.Add(new Node() { PreLink = Queue[index].CurLink, CurLink = @"C:\Users\Thanh\Desktop\Test\" + t });
+                            flag = false;
+                        }
+                    }
+
+                }
+                if (temp.Count != 0)
+                {
+                    Queue.Remove(Queue[index]);
+                }
+                if (temp.Count == 0)
+                {
+                    index++;
+                }
+            }
+            var tempFlag = true;
+            var tempPath = Path[Path.Count - 1].PreLink;
+            List<string> Paths = new List<string>();
+            Paths.Add(Path[Path.Count - 1].PreLink.Replace(@"C:\Users\Thanh\Desktop\Test\", "") + "->" + Path[Path.Count - 1].CurLink.Replace(@"C:\Users\Thanh\Desktop\Test\", "") + Environment.NewLine);
+            while (tempFlag)
+            {
+                foreach (var x in Path)
+                {
+                    if (x.CurLink == tempPath)
+                    {
+                        Paths.Add(x.PreLink.Replace(@"C:\Users\Thanh\Desktop\Test\", "") + "->" + x.CurLink.Replace(@"C:\Users\Thanh\Desktop\Test\", "") + Environment.NewLine);
+                        tempPath = x.PreLink;
+                        if (x.PreLink.Replace(@"C:\Users\Thanh\Desktop\Test\", "") == "1.html")
+                        {
+                            tempFlag = false;
+                            break;
+                        }
+                        break;
+                    }              
+                }
+            }         
+            Paths.Reverse();
+            foreach(var path in Paths)
+            {
+                linkTextBox.Text += path;
+            }    
+        }
+        public static bool CheckInPath(string path, List<Node> Paths)
+        {
+            foreach (var node in Paths)
+            {
+                if (@"C:\Users\Thanh\Desktop\Test\" + path == node.PreLink)
+                    return false;
+            }
+            return true;
+        }
+        public static List<string> docketqua(string fileName)
+        {
+            //string s = "";
+            List<string> link = new List<string>();
+            //OpenFileDialog openFileDialog1 = new OpenFileDialog
+            //{
+            //    InitialDirectory = @"C:\",
+            //    Title = "Browse Text Files",
+
+            //    CheckFileExists = true,
+            //    CheckPathExists = true,
+
+            //    DefaultExt = "txt",
+            //    Filter = "html files (*.html)|*.html",
+            //    FilterIndex = 2,
+            //    RestoreDirectory = true,
+
+            //    ReadOnlyChecked = true,
+            //    ShowReadOnly = true
+            //};
+
+            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            //{
+            //    s = openFileDialog1.FileName;
+            //}
+            if (fileName != "")
+            {
+                String input = File.ReadAllText(fileName);
+                foreach (var row in input.Split('\n'))
+                {
+                    if (row.Contains("<a"))
+                    {
+                        string temp = "";
+                        var href = row.IndexOf("href");
+                        var html = row.IndexOf(".html");
+                        for (int i = href + 6; i < html; i++)
+                        {
+                            temp += row[i];
+                        }
+                        link.Add(temp + ".html");
+                    }
+                }
+                return link;
+            }
+            return null;
+        }
+
     }
 }
