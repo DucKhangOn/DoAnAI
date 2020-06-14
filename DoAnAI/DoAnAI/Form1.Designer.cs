46c461af6622b540854dd0145555f6e641a5d25e
Ngoc Thanh <yasuo12091999@gmail.com>
2020-06-14 20:31:24 +0700

    Aaaaaaaa

--- DoAnAI/DoAnAI/Form1.Designer.cs
+++ DoAnAI/DoAnAI/Form1.Designer.cs
@@ -35,14 +35,16 @@
             this.sourceTextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.dfsSearchButton = new System.Windows.Forms.Button();
+            this.iddfsButton = new System.Windows.Forms.Button();
+            this.bidirectionalSearch = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // readFileButton
             // 
-            this.readFileButton.Location = new System.Drawing.Point(297, 302);
-            this.readFileButton.Margin = new System.Windows.Forms.Padding(4);
+            this.readFileButton.Location = new System.Drawing.Point(75, 247);
             this.readFileButton.Name = "readFileButton";
-            this.readFileButton.Size = new System.Drawing.Size(140, 81);
+            this.readFileButton.Size = new System.Drawing.Size(105, 66);
             this.readFileButton.TabIndex = 0;
             this.readFileButton.Text = "BFS";
             this.readFileButton.UseVisualStyleBackColor = true;
@@ -50,65 +52,91 @@
             // 
             // linkTextBox
             // 
-            this.linkTextBox.Location = new System.Drawing.Point(221, 26);
-            this.linkTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.linkTextBox.Location = new System.Drawing.Point(295, 12);
             this.linkTextBox.Multiline = true;
             this.linkTextBox.Name = "linkTextBox";
-            this.linkTextBox.Size = new System.Drawing.Size(541, 203);
+            this.linkTextBox.Size = new System.Drawing.Size(407, 166);
             this.linkTextBox.TabIndex = 1;
             // 
             // goalLinkTextBox
             // 
-            this.goalLinkTextBox.Location = new System.Drawing.Point(624, 255);
-            this.goalLinkTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.goalLinkTextBox.Location = new System.Drawing.Point(468, 207);
             this.goalLinkTextBox.Name = "goalLinkTextBox";
-            this.goalLinkTextBox.Size = new System.Drawing.Size(139, 22);
+            this.goalLinkTextBox.Size = new System.Drawing.Size(105, 20);
             this.goalLinkTextBox.TabIndex = 2;
+            this.goalLinkTextBox.Text = " ";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(545, 258);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(409, 210);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Đích đến";
             // 
             // sourceTextBox
             // 
-            this.sourceTextBox.Location = new System.Drawing.Point(297, 250);
-            this.sourceTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.sourceTextBox.Location = new System.Drawing.Point(223, 203);
             this.sourceTextBox.Name = "sourceTextBox";
-            this.sourceTextBox.Size = new System.Drawing.Size(139, 22);
+            this.sourceTextBox.Size = new System.Drawing.Size(105, 20);
             this.sourceTextBox.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(219, 254);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(164, 206);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Nguồn";
             // 
             // dfsSearchButton
             // 
-            this.dfsSearchButton.Location = new System.Drawing.Point(622, 302);
-            this.dfsSearchButton.Margin = new System.Windows.Forms.Padding(4);
+            this.dfsSearchButton.Location = new System.Drawing.Point(399, 246);
             this.dfsSearchButton.Name = "dfsSearchButton";
-            this.dfsSearchButton.Size = new System.Drawing.Size(140, 81);
+            this.dfsSearchButton.Size = new System.Drawing.Size(105, 66);
             this.dfsSearchButton.TabIndex = 4;
             this.dfsSearchButton.Text = "DFS";
             this.dfsSearchButton.UseVisualStyleBackColor = true;
             this.dfsSearchButton.Click += new System.EventHandler(this.dfsSearchButton_Click);
             // 
+            // iddfsButton
+            // 
+            this.iddfsButton.Location = new System.Drawing.Point(240, 248);
+            this.iddfsButton.Name = "iddfsButton";
+            this.iddfsButton.Size = new System.Drawing.Size(88, 65);
+            this.iddfsButton.TabIndex = 5;
+            this.iddfsButton.Text = "IDDFS";
+            this.iddfsButton.UseVisualStyleBackColor = true;
+            this.iddfsButton.Click += new System.EventHandler(this.iddfsButton_Click);
+            // 
+            // bidirectionalSearch
+            // 
+            this.bidirectionalSearch.Location = new System.Drawing.Point(583, 245);
+            this.bidirectionalSearch.Name = "bidirectionalSearch";
+            this.bidirectionalSearch.Size = new System.Drawing.Size(101, 65);
+            this.bidirectionalSearch.TabIndex = 6;
+            this.bidirectionalSearch.Text = "BDS";
+            this.bidirectionalSearch.UseVisualStyleBackColor = true;
+            this.bidirectionalSearch.Click += new System.EventHandler(this.bidirectionalSearch_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(250, 185);
+            this.webBrowser.TabIndex = 7;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(985, 396);
+            this.ClientSize = new System.Drawing.Size(739, 322);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.bidirectionalSearch);
+            this.Controls.Add(this.iddfsButton);
             this.Controls.Add(this.dfsSearchButton);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -116,7 +144,6 @@
             this.Controls.Add(this.goalLinkTextBox);
             this.Controls.Add(this.linkTextBox);
             this.Controls.Add(this.readFileButton);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -133,6 +160,9 @@
         private System.Windows.Forms.TextBox sourceTextBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button dfsSearchButton;
+        private System.Windows.Forms.Button iddfsButton;
+        private System.Windows.Forms.Button bidirectionalSearch;
+        private System.Windows.Forms.WebBrowser webBrowser;
     }
 }
 
--- DoAnAI/DoAnAI/Form1.cs
+++ DoAnAI/DoAnAI/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,6 +7,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,7 +17,7 @@ namespace DoAnAI
 
     public partial class Form1 : Form
     {
-        public const string SourceDir = @"C:\Users\KhangOD\Desktop\Test\";
+        public const string SourceDir = @"C:\Users\Thanh\Desktop\Test\";
         public struct Node
         {
             public string PreLink;
@@ -23,7 +25,11 @@ namespace DoAnAI
         };
         List<Node> Queue;
         List<Node> Path;
+        List<Node> PathForward;
+        List<Node> PathBackward;
         Stack<Node> stackNode;
+        Queue<Node> QueueForward;
+        Queue<Node> QueueBackward;
         public Form1()
         {
             InitializeComponent();
@@ -38,6 +44,20 @@ namespace DoAnAI
             var flag = true;
             int index = 0;
             var s = docketqua(SourceDir + sourceTextBox.Text);
+            string html = ReadFileHtml(SourceDir + sourceTextBox.Text);
+            webBrowser.Navigate(SourceDir + sourceTextBox.Text);
+            //webBrowser.DocumentText = "0";
+            //webBrowser.Document.OpenNew(true);
+            //webBrowser.Document.Write(html);
+            //webBrowser.Refresh();
+            //HtmlElementCollection links = webBrowser.Document.GetElementsByTagName("A");
+            //foreach (HtmlElement link in links)
+            //{
+            //    if (link.InnerText.Equals("2"))
+            //    {
+            //        link.InvokeMember("Click");
+            //    }
+            //}
             foreach (var x in s)
             {
                 if (x == goalLinkTextBox.Text)
@@ -112,6 +132,11 @@ namespace DoAnAI
             }
             return true;
         }
+        public string ReadFileHtml(string path)
+        {
+            var file = File.ReadAllText(path);
+            return file;
+        }
         public static List<string> docketqua(string fileName)
         {
             List<string> link = new List<string>();
@@ -139,11 +164,11 @@ namespace DoAnAI
 
         public static void ghiketqua(List<Node> Path)
         {
-            string filePath = "$"+SourceDir+"path.txt";
-            TextWriter writer = new StreamWriter($@"C:\Users\KhangOD\Desktop\Test\path.txt");
+            string filePath = "$" + SourceDir + "path.txt";
+            TextWriter writer = new StreamWriter($@"C:\Users\Thanh\Desktop\Test\path.txt");
             foreach (Node node in Path)
-            { 
-                writer.WriteLine(node.PreLink + " -> " + node.CurLink); 
+            {
+                writer.WriteLine(node.PreLink + " -> " + node.CurLink);
             }
             writer.Close();
         }
@@ -254,6 +279,91 @@ namespace DoAnAI
                 return Paths;
             }
             return null;
+        }
+
+        private void iddfsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bidirectionalSearch_Click(object sender, EventArgs e)
+        {
+            QueueForward = new Queue<Node>();
+            QueueBackward = new Queue<Node>();
+            PathForward = new List<Node>();
+            PathBackward = new List<Node>();
+            linkTextBox.Clear();
+            var flag = true;
+            var source = docketqua(SourceDir + sourceTextBox.Text);
+            var des = docketqua(SourceDir + goalLinkTextBox.Text);
+            foreach (var s in source)
+            {
+                if (s == goalLinkTextBox.Text)
+                {
+                    linkTextBox.Text += sourceTextBox.Text + "->" + s;
+                    flag = false;
+                    break;
+                }
+                var temp = docketqua(SourceDir + s);
+                if (temp.Contains(SourceDir + sourceTextBox.Text))
+                {
+                    QueueForward.Enqueue(new Node() { PreLink = SourceDir + sourceTextBox.Text, CurLink = SourceDir + s });
+                }
+            }
+            foreach (var d in des)
+            {
+                if (d == sourceTextBox.Text)
+                {
+                    if (source.Contains(SourceDir + d))
+                    {
+                        linkTextBox.Text += sourceTextBox + "->" + d;
+                        flag = false;
+                        break;
+                    }
+                }
+                var temp = docketqua(SourceDir + d);
+                if (temp.Contains(SourceDir + goalLinkTextBox.Text))
+                {
+                    QueueForward.Enqueue(new Node() { PreLink = SourceDir + goalLinkTextBox.Text, CurLink = SourceDir + d });
+
+                }
+            }
+            while (flag)
+            {
+                if (QueueForward.Count == 0 || QueueBackward.Count == 0)
+                {
+                    break;
+                }
+                var forward = QueueForward.Peek();
+                var backward = QueueBackward.Peek();
+                var forwardAble = docketqua(SourceDir + forward);
+                var backwardAble = docketqua(SourceDir + backward);
+                if (CheckInPath(forward.CurLink, Path))
+                {
+                    PathForward.Add(forward);
+                }
+                foreach (var fa in forwardAble)
+                {
+                    var temp = docketqua(SourceDir + fa);
+                    foreach(var t in temp)
+                    {
+                        if(t == SourceDir + goalLinkTextBox.Text)
+                        {
+                            flag = false;
+                            PathForward.Add(new Node() { PreLink = SourceDir + fa,CurLink = SourceDir + t});
+                            break;
+                        }
+                        if(t != SourceDir + sourceTextBox.Text)
+                        { 
+                            if(t == SourceDir + forward.CurLink)
+                            {
+                                QueueForward.Enqueue(new Node() { PreLink = SourceDir + forward.CurLink, CurLink = SourceDir + t });
+                            }
+                        }
+                    }
+                }
+                QueueForward.Dequeue();
+            }
         }
     }
 }
