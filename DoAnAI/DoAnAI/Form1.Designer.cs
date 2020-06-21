be24491d6627222373ed711f93b6252bfc0dc54c
Duc Khang <javaneverdie0612@gmail.com>
2020-06-21 22:26:57 +0700

    Revert "muadiiiii"
    This reverts commit db1535e973c750fcbb82a4b073da602485aeacce.

--- DoAnAI/DoAnAI/Form1.Designer.cs
+++ DoAnAI/DoAnAI/Form1.Designer.cs
@@ -38,15 +38,13 @@
             this.iddfsButton = new System.Windows.Forms.Button();
             this.bidirectionalSearch = new System.Windows.Forms.Button();
             this.webBrowser = new System.Windows.Forms.WebBrowser();
-            this.bntNext = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // readFileButton
             // 
-            this.readFileButton.Location = new System.Drawing.Point(100, 304);
-            this.readFileButton.Margin = new System.Windows.Forms.Padding(4);
+            this.readFileButton.Location = new System.Drawing.Point(75, 247);
             this.readFileButton.Name = "readFileButton";
-            this.readFileButton.Size = new System.Drawing.Size(140, 81);
+            this.readFileButton.Size = new System.Drawing.Size(105, 66);
             this.readFileButton.TabIndex = 0;
             this.readFileButton.Text = "BFS";
             this.readFileButton.UseVisualStyleBackColor = true;
@@ -54,56 +52,50 @@
             // 
             // linkTextBox
             // 
-            this.linkTextBox.Location = new System.Drawing.Point(393, 15);
-            this.linkTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.linkTextBox.Location = new System.Drawing.Point(295, 12);
             this.linkTextBox.Multiline = true;
             this.linkTextBox.Name = "linkTextBox";
-            this.linkTextBox.Size = new System.Drawing.Size(541, 203);
+            this.linkTextBox.Size = new System.Drawing.Size(407, 166);
             this.linkTextBox.TabIndex = 1;
             // 
             // goalLinkTextBox
             // 
-            this.goalLinkTextBox.Location = new System.Drawing.Point(624, 255);
-            this.goalLinkTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.goalLinkTextBox.Location = new System.Drawing.Point(468, 207);
             this.goalLinkTextBox.Name = "goalLinkTextBox";
-            this.goalLinkTextBox.Size = new System.Drawing.Size(139, 22);
+            this.goalLinkTextBox.Size = new System.Drawing.Size(105, 20);
             this.goalLinkTextBox.TabIndex = 2;
             this.goalLinkTextBox.Text = " ";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(545, 258);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(409, 210);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(64, 17);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Đích đến";
             // 
             // sourceTextBox
             // 
-            this.sourceTextBox.Location = new System.Drawing.Point(297, 250);
-            this.sourceTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.sourceTextBox.Location = new System.Drawing.Point(223, 203);
             this.sourceTextBox.Name = "sourceTextBox";
-            this.sourceTextBox.Size = new System.Drawing.Size(139, 22);
+            this.sourceTextBox.Size = new System.Drawing.Size(105, 20);
             this.sourceTextBox.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(219, 254);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(164, 206);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.Size = new System.Drawing.Size(39, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Nguồn";
             // 
             // dfsSearchButton
             // 
-            this.dfsSearchButton.Location = new System.Drawing.Point(532, 303);
-            this.dfsSearchButton.Margin = new System.Windows.Forms.Padding(4);
+            this.dfsSearchButton.Location = new System.Drawing.Point(399, 246);
             this.dfsSearchButton.Name = "dfsSearchButton";
-            this.dfsSearchButton.Size = new System.Drawing.Size(140, 81);
+            this.dfsSearchButton.Size = new System.Drawing.Size(105, 66);
             this.dfsSearchButton.TabIndex = 4;
             this.dfsSearchButton.Text = "DFS";
             this.dfsSearchButton.UseVisualStyleBackColor = true;
@@ -111,10 +103,9 @@
             // 
             // iddfsButton
             // 
-            this.iddfsButton.Location = new System.Drawing.Point(320, 305);
-            this.iddfsButton.Margin = new System.Windows.Forms.Padding(4);
+            this.iddfsButton.Location = new System.Drawing.Point(240, 248);
             this.iddfsButton.Name = "iddfsButton";
-            this.iddfsButton.Size = new System.Drawing.Size(117, 80);
+            this.iddfsButton.Size = new System.Drawing.Size(88, 65);
             this.iddfsButton.TabIndex = 5;
             this.iddfsButton.Text = "IDDFS";
             this.iddfsButton.UseVisualStyleBackColor = true;
@@ -122,10 +113,9 @@
             // 
             // bidirectionalSearch
             // 
-            this.bidirectionalSearch.Location = new System.Drawing.Point(777, 302);
-            this.bidirectionalSearch.Margin = new System.Windows.Forms.Padding(4);
+            this.bidirectionalSearch.Location = new System.Drawing.Point(583, 245);
             this.bidirectionalSearch.Name = "bidirectionalSearch";
-            this.bidirectionalSearch.Size = new System.Drawing.Size(135, 80);
+            this.bidirectionalSearch.Size = new System.Drawing.Size(101, 65);
             this.bidirectionalSearch.TabIndex = 6;
             this.bidirectionalSearch.Text = "BDS";
             this.bidirectionalSearch.UseVisualStyleBackColor = true;
@@ -133,29 +123,17 @@
             // 
             // webBrowser
             // 
-            this.webBrowser.Location = new System.Drawing.Point(16, 15);
-            this.webBrowser.Margin = new System.Windows.Forms.Padding(4);
-            this.webBrowser.MinimumSize = new System.Drawing.Size(27, 25);
+            this.webBrowser.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser.Name = "webBrowser";
-            this.webBrowser.Size = new System.Drawing.Size(333, 228);
+            this.webBrowser.Size = new System.Drawing.Size(250, 185);
             this.webBrowser.TabIndex = 7;
-            // 
-            // bntNext
-            // 
-            this.bntNext.Location = new System.Drawing.Point(16, 250);
-            this.bntNext.Name = "bntNext";
-            this.bntNext.Size = new System.Drawing.Size(75, 23);
-            this.bntNext.TabIndex = 8;
-            this.bntNext.Text = "Check";
-            this.bntNext.UseVisualStyleBackColor = true;
-            this.bntNext.Click += new System.EventHandler(this.bntNext_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(985, 396);
-            this.Controls.Add(this.bntNext);
+            this.ClientSize = new System.Drawing.Size(739, 322);
             this.Controls.Add(this.webBrowser);
             this.Controls.Add(this.bidirectionalSearch);
             this.Controls.Add(this.iddfsButton);
@@ -166,7 +144,6 @@
             this.Controls.Add(this.goalLinkTextBox);
             this.Controls.Add(this.linkTextBox);
             this.Controls.Add(this.readFileButton);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -186,7 +163,6 @@
         private System.Windows.Forms.Button iddfsButton;
         private System.Windows.Forms.Button bidirectionalSearch;
         private System.Windows.Forms.WebBrowser webBrowser;
-        private System.Windows.Forms.Button bntNext;
     }
 }
 
